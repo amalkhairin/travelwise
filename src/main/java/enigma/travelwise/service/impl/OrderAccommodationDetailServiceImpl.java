12334f34ea3e4e66848ce261482130861d3a17a9
AthallahRamadani <athallahramadani21@gmail.com>
2024-07-23 16:23:17 +0700

    #5 write inside OrderAccommodationServiceImpl and fix implement

--- src/main/java/enigma/travelwise/service/impl/OrderAccommodationDetailServiceImpl.java
+++ src/main/java/enigma/travelwise/service/impl/OrderAccommodationDetailServiceImpl.java
@@ -1,24 +1,28 @@
 package enigma.travelwise.service.impl;
 
 import enigma.travelwise.model.OrderAccommodation;
+import enigma.travelwise.model.OrderAccommodationDetail;
+import enigma.travelwise.service.OrderAccommodationDetailService;
 import enigma.travelwise.service.OrderAccommodationService;
 import enigma.travelwise.utils.dto.OrderAccommodationDTO;
+import enigma.travelwise.utils.dto.OrderAccommodationDetailDTO;
 
 import java.util.List;
 
-public class OrderAccommodationDetailServiceImpl implements OrderAccommodationService {
+public class OrderAccommodationDetailServiceImpl implements OrderAccommodationDetailService {
+
     @Override
-    public OrderAccommodation create(OrderAccommodationDTO request) {
+    public OrderAccommodationDetail create(OrderAccommodationDetailDTO request) {
         return null;
     }
 
     @Override
-    public List<OrderAccommodation> getAll() {
+    public List<OrderAccommodationDetail> getAll() {
         return List.of();
     }
 
     @Override
-    public OrderAccommodation getOne(Long id) {
+    public OrderAccommodationDetail getOne(Long id) {
         return null;
     }
 }
--- src/main/java/enigma/travelwise/service/impl/OrderAccommodationServiceImpl.java
+++ src/main/java/enigma/travelwise/service/impl/OrderAccommodationServiceImpl.java
@@ -1,24 +1,47 @@
 package enigma.travelwise.service.impl;
 
-import enigma.travelwise.model.OrderAccommodationDetail;
+import enigma.travelwise.model.OrderAccommodation;
+import enigma.travelwise.model.UserEntity;
+import enigma.travelwise.repository.OrderAccommodationRepository;
 import enigma.travelwise.service.OrderAccommodationDetailService;
+import enigma.travelwise.service.OrderAccommodationService;
+import enigma.travelwise.utils.dto.OrderAccommodationDTO;
 import enigma.travelwise.utils.dto.OrderAccommodationDetailDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public class OrderAccommodationServiceImpl implements OrderAccommodationDetailService {
+@Service
+@RequiredArgsConstructor
+public class OrderAccommodationServiceImpl implements OrderAccommodationService {
+    private final OrderAccommodationRepository orderAccommodationRepository;
+    private final OrderAccommodationDetailService orderAccommodationDetailService;
+
     @Override
-    public OrderAccommodationDetail create(OrderAccommodationDetailDTO request) {
-        return null;
+    public OrderAccommodation create(OrderAccommodationDTO request) {
+//        UserEntity user = userService.getOne(request.getUserId());
+        List<OrderAccommodationDetailDTO> details = request.getOrderAccommodationDetails();
+        OrderAccommodation newOrderAccommodation = new OrderAccommodation();
+//        newOrderAccommodation.setUser(user);
+
+        OrderAccommodation result = orderAccommodationRepository.save(newOrderAccommodation);
+
+        for (OrderAccommodationDetailDTO detail : details) {
+            detail.setOrderAccommodation(result);
+            orderAccommodationDetailService.create(detail);
+        }
+
+        return result;
     }
 
     @Override
-    public List<OrderAccommodationDetail> getAll() {
-        return List.of();
+    public List<OrderAccommodation> getAll() {
+        return orderAccommodationRepository.findAll();
     }
 
     @Override
-    public OrderAccommodationDetail getOne(Long id) {
-        return null;
+    public OrderAccommodation getOne(Long id) {
+        return orderAccommodationRepository.findById(id).orElseThrow(() -> new RuntimeException("Order not found"));
     }
 }
