ab6bf5a479e24149c3b347628893a839deecd6a1
amalkhairin <amallanch45@gmail.com>
2024-07-23 16:41:51 +0700

    feat: add create method for destination service

--- src/main/java/enigma/travelwise/service/impl/DestinationServiceImpl.java
+++ src/main/java/enigma/travelwise/service/impl/DestinationServiceImpl.java
@@ -2,23 +2,44 @@ package enigma.travelwise.service.impl;
 
 import enigma.travelwise.model.Destination;
 import enigma.travelwise.repository.DestinationRepository;
+import enigma.travelwise.service.CloudinaryService;
 import enigma.travelwise.service.DestinationService;
 import enigma.travelwise.utils.dto.DestionationDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class DestinationServiceImpl implements DestinationService {
+    private final CloudinaryService cloudinaryService;
     private final DestinationRepository destinationRepository;
 
     @Override
     public Destination create(DestionationDTO req) {
-        return null;
+        Destination destination = Destination.builder()
+                .name(req.getName())
+                .categories(req.getCategories())
+                .description(req.getDescription())
+                .locations(req.getLocations())
+                .category_prices(req.getCategory_prices())
+                .latitude(req.getLatitude())
+                .longitude(req.getLongitude())
+                .build();
+        Map<String, String> map_url = new HashMap<>();
+        for (int i = 0; i < req.getPictures().size(); i++) {
+            String url = cloudinaryService.uploadFile(req.getPictures().get(i), "travelwise_destinations");
+            map_url.put("pict_" + i, url);
+        }
+
+        destination.setPictures(map_url);
+        return destinationRepository.save(destination);
+
     }
 
     @Override
