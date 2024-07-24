95f5a402930ae4cd3a3ac52e397ebdae24745d87
nabilasucisa <nabila.s.syabani@gmail.com>
2024-07-24 10:49:43 +0700

    finishing user until user controller, add response package

--- src/main/java/enigma/travelwise/service/AccommodationService.java
+++ src/main/java/enigma/travelwise/service/AccommodationService.java
@@ -1,16 +1,16 @@
 package enigma.travelwise.service;
 
 import enigma.travelwise.model.Accommodation;
+import enigma.travelwise.utils.dto.AccommodationChangePictureDTO;
 import enigma.travelwise.utils.dto.AccommodationDTO;
-import org.springframework.http.ResponseEntity;
 
 import java.util.List;
-import java.util.Map;
 
 public interface AccommodationService {
-    ResponseEntity<Map> create(AccommodationDTO request);
-    List<Accommodation> getAll();
-    Accommodation getById(Integer id);
-    ResponseEntity<Map> update(AccommodationDTO request, Integer id);
-    void deleteById(Integer id);
+    Accommodation create(AccommodationDTO request);
+    List<Accommodation> getAll(String name, String location, String category);
+    Accommodation getById(Long id);
+    Accommodation update(AccommodationDTO request, Long id);
+    void changePicture(AccommodationChangePictureDTO request, Long id);
+    void deleteById(Long id);
 }
--- src/main/java/enigma/travelwise/service/impl/UserServiceImpl.java
+++ src/main/java/enigma/travelwise/service/impl/UserServiceImpl.java
@@ -4,14 +4,17 @@ import enigma.travelwise.model.UserEntity;
 import enigma.travelwise.repository.UserRepository;
 import enigma.travelwise.service.CloudinaryService;
 import enigma.travelwise.service.UserService;
+import enigma.travelwise.utils.dto.UserChangeLocationDTO;
+import enigma.travelwise.utils.dto.UserChangeProfilePictureDTO;
 import enigma.travelwise.utils.dto.UserDTO;
+import enigma.travelwise.utils.dto.UserUpdateDTO;
 import enigma.travelwise.utils.specification.UserSpecification;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -46,17 +49,51 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public UserEntity update(UserDTO request, Long id) {
+    public UserEntity update(UserUpdateDTO request, Long id) {
         UserEntity updatedUser = this.getById(id);
         updatedUser.setName(request.getName());
         updatedUser.setEmail(request.getEmail());
         updatedUser.setPassword(request.getPassword());
         updatedUser.setPhone_number(request.getPhone_number());
-        updatedUser.setProfile_picture(cloudinaryService.uploadFile(request.getProfile_picture(), "travelwise_user"));
-        // Latitude Longitude tidak diganti, sama seperti yang sebelumnya
+        // Profile Picture, Latitude Longitude tidak diganti, sama seperti yang sebelumnya
+        // Karena ada fitur lain untuk mengganti profile picture dan location dari user
+        updatedUser.setProfile_picture(updatedUser.getProfile_picture());
         updatedUser.setLatitude(updatedUser.getLatitude());
         updatedUser.setLongitude(updatedUser.getLongitude());
         return userRepository.save(updatedUser);
+    }
+
+    @Override
+    public String changeProfilePicture(UserChangeProfilePictureDTO request, Long id) {
+        UserEntity changeProfile = this.getById(id);
+        changeProfile.setProfile_picture(cloudinaryService.uploadFile(request.getProfile_picture(), "travelwise_user"));
+        // yang lainnya tetap sama karena sudah diprovide di fitur update
+        changeProfile.setName(changeProfile.getName());
+        changeProfile.setEmail(changeProfile.getEmail());
+        changeProfile.setPassword(changeProfile.getPassword());
+        changeProfile.setPhone_number(changeProfile.getPhone_number());
+        changeProfile.setLatitude(changeProfile.getLatitude());
+        changeProfile.setLongitude(changeProfile.getLongitude());
+        userRepository.save(changeProfile);
+        String url = changeProfile.getProfile_picture();
+        String result = "New profile picture : " + url;
+        return result;
+    }
+
+    @Override
+    public String changeLocation(UserChangeLocationDTO request, Long id) {
+        UserEntity changeLocation = this.getById(id);
+        changeLocation.setLatitude(request.getLatitude());
+        changeLocation.setLongitude(request.getLongitude());
+        // yang lainnya tetap sama karena sudah diprovide di fitur update
+        changeLocation.setName(changeLocation.getName());
+        changeLocation.setEmail(changeLocation.getEmail());
+        changeLocation.setPassword(changeLocation.getPassword());
+        changeLocation.setPhone_number(changeLocation.getPhone_number());
+        changeLocation.setProfile_picture(changeLocation.getProfile_picture());
+        userRepository.save(changeLocation);
+        String result = "Newest location => latitude = " + changeLocation.getLatitude() + " | longitude = " + changeLocation.getLongitude();
+        return result;
     }
 
     @Override
